e577ff9e56b170ea1a4423198642d4a28309d92b
Luv Agarwal <agarwal.iiit@gmail.com>
2015-10-06 22:26:12 +0530

    some changes

--- main.py
+++ main.py
@@ -4,22 +4,23 @@ import utils
 import similarity_functions as sf
 np.set_printoptions(threshold='nan')
 
-def main():
+def main(load=True):
     training, test, metadata = parse.load(1)
-    '''
-    ratingMatrix = utils.constructRatingMatrix(training, metadata)
-    similarity = sf.cosineMatrix(ratingMatrix)
-    np.savetxt('similarity.txt', similarity)
-    print "similarity done"
-    prediction = utils.predictRating(similarity, ratingMatrix)
-    np.savetxt('prediction.txt', prediction)
-    print "prediction done"
+    if not load:
+        ratingMatrix = utils.constructRatingMatrix(training, metadata)
+        similarity = sf.cosineMatrix(ratingMatrix)
+        np.savetxt('similarity.txt', similarity)
+        print "similarity done"
+        prediction = utils.predictRating(similarity, ratingMatrix)
+        np.savetxt('prediction.txt', prediction)
+        print "prediction done"
+    else:
+        with open('similarity.txt') as f:
+            similarity = np.loadtxt(f)
+        with open('prediction.txt') as f:
+            prediction = np.loadtxt(f)
+    
     import pdb; pdb.set_trace()
-    '''
-    with open('similarity.txt') as f:
-        similarity = np.loadtxt(f)
-    with open('prediction.txt') as f:
-        prediction = np.loadtxt(f)
     #import pdb; pdb.set_trace();
     predictionOnTest = prediction[test[:, 0].astype(int) - 1, test[:, 1].astype(int)-1]
     error = predictionOnTest - test[:, 2]
