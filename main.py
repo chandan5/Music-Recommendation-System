9a8958e0be924dedff37ae389e9a334005bb852d
Chandan Kharbanda <chandan.kharbanda5@gmail.com>
2015-10-06 22:14:24 +0530

    some more changes

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ np.set_printoptions(threshold='nan')
 
 def main():
     training, test, metadata = parse.load(1)
+    '''
     ratingMatrix = utils.constructRatingMatrix(training, metadata)
     similarity = sf.cosineMatrix(ratingMatrix)
     np.savetxt('similarity.txt', similarity)
@@ -14,9 +15,15 @@ def main():
     np.savetxt('prediction.txt', prediction)
     print "prediction done"
     import pdb; pdb.set_trace()
-    predictionOnTest = prediction[test[:, 0]-1, test[:, 1]-1]
+    '''
+    with open('similarity.txt') as f:
+        similarity = np.loadtxt(f)
+    with open('prediction.txt') as f:
+        prediction = np.loadtxt(f)
+    #import pdb; pdb.set_trace();
+    predictionOnTest = prediction[test[:, 0].astype(int) - 1, test[:, 1].astype(int)-1]
     error = predictionOnTest - test[:, 2]
-    print np.abs(error).mean()
+    return np.abs(error).mean()
 
 if __name__ == '__main__':
     print main()
