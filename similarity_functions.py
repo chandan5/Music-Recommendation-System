7b3ed2f82eb909013355b3b8005c2c1d38e1820b
Luv Agarwal <agarwal.iiit@gmail.com>
2015-10-06 20:27:22 +0530

    cosine modified

--- similarity_functions.py
+++ similarity_functions.py
@@ -1,24 +1,12 @@
 import numpy as np
-import math
 
 def cosine(a, b):
-    cosine_ret = 0
-    a_norm_special = 0
-    b_norm_special = 0
-    length = (a.shape)[0]
-    for i in xrange(length):
-        if(a[i] != 0 and b[i] !=0):
-            cosine_ret += a[i]*b[i]
-            a_norm_special += a[i]*a[i]
-            b_norm_special += b[i]*b[i]
-    if(a_norm_special != 0 and b_norm_special != 0):
-        a_norm_special = math.sqrt(a_norm_special)
-        b_norm_special = math.sqrt(b_norm_special)
-        cosine_ret /= a_norm_special
-        cosine_ret /= b_norm_special
-    return cosine_ret
-    #import pdb; pdb.set_trace()        
-    #return np.dot(a, b)/(np.linalg.norm(a)*np.linalg.norm(b))
+    dp = np.dot(a, b)
+    p = a*b
+    norm = np.linalg.norm(p/b)*np.linalg.norm(p/a)
+    if not norm:
+        return 0
+    return dp/norm
 
 def cosineMatrix(ratingMatrix, noitems):
     sim = np.zeros((noitems, noitems))
@@ -26,7 +14,6 @@ def cosineMatrix(ratingMatrix, noitems):
         for j in xrange(i,noitems):
             sim[i][j] = cosine(ratingMatrix[:, i],ratingMatrix[:, j])
             sim[j][i] = sim[i][j]
-        print i
     return sim
 
 # need to modify below 2 functions IMPORTANT
